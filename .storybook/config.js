3989c7093775d8ee952f6754a59645725b49dd22
Daniel Lewis <dan@rootsystems.nz>
2018-07-23 19:02:00 +1200

    update storybook settings

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,18 +1,41 @@
-// TODO https://github.com/storybooks/storybook/blob/master/examples/official-storybook/config.js
+import { configure } from '@storybook/react'
+import { themes } from '@storybook/components'
+import { setOptions } from '@storybook/addon-options'
+import { configureViewport, INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 
-import { configure } from '@storybook/react';
+import extraViewports from './extra-viewports.json'
+
+setOptions({
+  name: 'Cobuy Atomic',
+  url: 'https://github.com/root-systems/cobuy-atomic',
+  goFullScreen: false,
+  showAddonsPanel: true,
+  showSearchBox: false,
+  addonPanelInRight: false,
+  sortStoriesByKind: false,
+  hierarchySeparator: /\/|\./,
+  hierarchyRootSeparator: /\|/,
+  enableShortcuts: false,
+  theme: themes.dark
+})
+
+configureViewport({
+  viewports: {
+    ...INITIAL_VIEWPORTS,
+    ...extraViewports,
+  },
+})
+
 
 function importAll(req) {
   req.keys().forEach(filename => req(filename));
 }
 
 function loadStories() {
-  let req;
-  req = require.context('../example/ui/components/atoms', true, /\.story.js$/);
-  importAll(req);
+  let req
 
- req = require.context('../ui/components/atoms', true, /\.story.js$/);
- importAll(req);
+  req = require.context('../', true, /\.story.js$/)
+  importAll(req)
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
