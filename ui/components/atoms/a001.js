2f3aede789d40f4f4a26d9ec7f44d50a2f481411
Richard Garcia <richardgarciaph@gmail.com>
2018-07-27 17:51:39 +1200

    update a001.js based on Dans feedback

--- ui/components/atoms/a001.js
+++ ui/components/atoms/a001.js
@@ -1,35 +1,14 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
 
+const styles = {}
 
-const styles = {
-  container: {
-    // backgroundColor: 'red',
-    padding: '1em'
-  },
-  primaryButton: {
-    color: 'green',
-    textDecoration: 'inherit',
-    '&:hover': {
-      textDecoration: 'underline',
-      backgroundColor: 'red'
-    },
-  }
-};
-
-const a001 = (props) => {
+const a001 = props => {
   return (
-    <div>
-      <div className = { props.classes.container }>
-        <button className = { props.classes.primaryButton } >{props.title}</button>
-      </div>
-      <div>
-        <Button variant={props.variant} color={props.buttonColor}>{props.title}</Button>
-      </div>
-    </div>
+    <Button variant={'contained'} color={props.buttonColor}>
+      {props.title}
+    </Button>
   )
 }
-
 export default withStyles(styles)(a001)
-  
