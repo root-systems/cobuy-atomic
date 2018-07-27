353104a2708103fb37dcbc20fa5a8d63d243e50a
richardgarcia <richardgarciaph@gmail.com>
2018-07-27 12:51:14 +1200

    add button samples

--- ui/components/atoms/a001.js
+++ ui/components/atoms/a001.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
 
 const styles = {
   container: {
-    backgroundColor: 'red',
+    // backgroundColor: 'red',
+    padding: '1em'
   },
+  primaryButton: {
+    color: 'green',
+    textDecoration: 'inherit',
+    '&:hover': {
+      textDecoration: 'underline',
+      backgroundColor: 'red'
+    },
+  }
 };
 
 const a001 = (props) => {
   return (
     <div>
-      <div className={props.classes.container}>
-        <button>{props.title}</button>
+      <div className = { props.classes.container }>
+        <button className = { props.classes.primaryButton } >{props.title}</button>
+      </div>
+      <div>
+        <Button variant={props.variant} color={props.buttonColor}>{props.title}</Button>
       </div>
     </div>
   )
 }
 
 export default withStyles(styles)(a001)
+  
