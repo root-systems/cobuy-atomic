8bb65740a1f7b7b72f5ccd43dfdb81fda1e8fff0
Daniel Lewis <dan@rootsystems.nz>
2018-07-30 09:04:13 +1200

    init icon-button

---
+++ ui/example/components/atoms/icon-button/stories.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withNotes } from '@storybook/addon-notes'
+
+import IconButton from '.'
+import notes from './notes.md';
+
+storiesOf('@example|atoms/IconButton', module)
+  .addDecorator(withNotes)
+  .add('Big Red', () => (
+    <IconButton />),
+    {notes: {markdown: notes}}
+  )
+  .add('disabled', () => (
+    <IconButton disabled={true} />),
+    {notes: {markdown: notes}}
+  )
---
+++ ui/example/components/atoms/icon-button/styles.js
@@ -0,0 +1,10 @@
+import theme from '../../../../theme'
+
+export default {
+  big: {
+    '--wired-icon-size': 50
+  },
+  red: {
+    '--wired-icon-bg-color': theme.palette.error.main
+  }
+}
