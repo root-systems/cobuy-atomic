91fb087714c2c15f230f13fd1be991ae75e25d31
Daniel Lewis <dan@rootsystems.nz>
2018-07-29 21:25:01 +1200

    rm children

--- ui/example/components/atoms/card/index.js
+++ ui/example/components/atoms/card/index.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import createPropTypes from 'json-schema-prop-types'
 import "wired-elements"
