11fa0e2081da00e168bf9b104edc5d4199973466
Daniel Lewis <dan@rootsystems.nz>
2018-07-29 18:43:00 +1200

    update input

--- ui/example/components/atoms/label/index.js
+++ ui/example/components/atoms/label/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import createPropTypes from 'json-schema-prop-types'
+
+import schema from './schema'
 
 const label = (props) => {
   return (
-    <label>{props.label}</label>
+    <h4>{props.label}</h4>
   )
 }
+
+label.prototype = createPropTypes(schema)
 
 export default label
--- ui/example/components/atoms/label/story.js
+++ ui/example/components/atoms/label/story.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withNotes } from '@storybook/addon-notes'
 
 import Label from '.'
+import notes from './notes.md';
 
 storiesOf('@example|atoms/Label', module)
-  .add('plain', () => (
-    <Label label={'Search the site'} />
-  ))
+  .addDecorator(withNotes)
+  .add('vanulla', () => (
+    <Label label={'Billing Address'}/>),
+    {notes: {markdown: notes}}
+  )
