96d357efff32982595261d9ed77b2f0303ae73bd
richardgarcia <richardgarciaph@gmail.com>
2018-07-31 12:16:46 +1200

    update fab button with 2 samples -- Add & Group, add notes

--- ui/components/atoms/a002/index.js
+++ ui/components/atoms/a002/index.js
@@ -2,18 +2,11 @@ import React from 'react'
 import createPropTypes from 'json-schema-prop-types'
 
 import Button from '@material-ui/core/Button';
-import Icon from '@material-ui/core/Icon';
-
 
 import schema from './schema'
 
 const a002 = (props) => {
-  return (
-    <div>
-      <Button variant={'fab'} color={ props.color } aria-label='Edit'><Icon>add</Icon></Button>
-    </div>
-   
-  )
+  return (<Button variant={'fab'} color={ props.color } aria-label={props.ariaLabel}>{props.icon}</Button>)
 }
 
 a002.propTypes = createPropTypes(schema)
--- ui/components/atoms/a002/stories.js
+++ ui/components/atoms/a002/stories.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes'
+
+import Icon from '@material-ui/core/Icon';
 
 import Button from '.'
+import notes from './notes.md';
 
 storiesOf('Atoms|A002', module)
-  .add('Default', () => (
-    <Button title={'default button'} color = {'default'} />
-  ))
-  .add('Primary', () => (
-    <Button title={'primary button'} color = {'primary'} />
-  ))
-  .add('Secondary', () => (
-    <Button title={'secondary button'} color ={'secondary'} />
-  ))
+  .addDecorator(withNotes)
+  .add('Add', () => 
+  <Button color={'default'} icon={<Icon>add_icon</Icon>}/>, {
+    notes: { markdown: notes }}
+  )
+  .add('Group', () => 
+  <Button color={'default'} icon={<Icon>group_icon</Icon>}/>, {
+    notes: { markdown: notes }}
+  )
