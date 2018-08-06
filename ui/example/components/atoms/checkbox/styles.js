9cea2f431a4506dcd00e3d409f028a3b9c85b536
Richard Garcia <richardgarciaph@gmail.com>
2018-08-07 11:33:33 +1200

    update theming code

--- ui/example/components/atoms/checkbox/styles.js
+++ ui/example/components/atoms/checkbox/styles.js
@@ -1,10 +1,11 @@
 import theme from '../../../../theme'
 
-export default {
+// export a function that has the current theme injected into it
+export default (theme) => ({
   error: {
     '--wired-checkbox-icon-color': theme.palette.error.main
   },
   done: {
     '--wired-checkbox-icon-color': 'green'
   }
-}
+})
