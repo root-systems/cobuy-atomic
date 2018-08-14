2704f81b0ca203ae6e7362e61864fc526db2d47a
Richard Garcia <richardgarciaph@gmail.com>
2018-08-14 13:32:14 +1200

    Clean M004 codes

---
+++ ui/components/molecules/M004-AppBar/stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+// Import index.js from the same folder
+import Button from '.'
+
+// List of stories of use cases for atom
+storiesOf('Molecules|M004 - AppBar', module)
+  .add('Default', () => (
+    <Button buttonTitle={'default button'} color={'default'} />
+  ))
