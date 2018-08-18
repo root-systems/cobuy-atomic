154a74ef0a4d18ff29cfae1eeec0b86873065a4a
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-18 08:44:30 +0200

    general tidy up of props

--- ui/components/atoms/A005-PageTitle/stories.js
+++ ui/components/atoms/A005-PageTitle/stories.js
@@ -2,16 +2,12 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withNotes } from '@storybook/addon-notes'
 
-import Typography from '.'
+import PageTitle from '.'
 
 import notes from './readme.md'
 
 storiesOf('Atoms|A005 - Page title', module)
   .addDecorator(withNotes)
-  .add(
-    'Groups domain',
-    () => (
-      <Typography title='Groups Domain' color='primary' alignment='center' />
-    ),
-    { notes: { markdown: notes } }
-  )
+  .add('Groups domain', () => <PageTitle>Groups Domain</PageTitle>, {
+    notes: { markdown: notes }
+  })
