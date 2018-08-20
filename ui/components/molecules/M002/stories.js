6914fbe2c349e46736d688c92f85b5fd5613f7b6
Richard Garcia <richardgarciaph@gmail.com>
2018-08-21 09:19:54 +1200

    remove m002 and m003

--- ui/components/molecules/M002/stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import Nav from '.'
-
-storiesOf('Molecules|M002', module).add('Default', () => (
-  <Nav title={`'First item'`} />
-))
