8df84fc12218736e41e11c8ec9bc573c9dced069
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-21 12:49:02 +0200

    init input with label

---
+++ ui/components/atoms/A010-InputWithLabel/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import createPropTypes from 'json-schema-prop-types'
+
+import {TextField} from '@material-ui/core'
+
+import schema from './schema'
+
+const A010 = props => {
+  return (
+    <TextField
+      id={props.id}
+      value={props.value}
+      type={props.type}
+      required={props.required}
+      disabled={props.disabled}
+      name={props.name}
+      label={props.label}
+      error={props.error}
+      placeholder={props.placeholder}
+      onChange={props.handleChange}
+      margin='normal'
+      InputProps={{
+        readOnly: props.readOnly
+      }}
+    />
+  )
+}
+
+A010.propTypes = createPropTypes(schema)
+
+export default A010
