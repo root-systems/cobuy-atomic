88880d1c5c66335464791cbbe7feb1b7cc0a1ea4
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-22 22:28:35 +0200

    remove uneeded props

--- ui/components/atoms/A004-AppBar/schema.js
+++ ui/components/atoms/A004-AppBar/schema.js
@@ -2,18 +2,10 @@ export default {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
   properties: {
-    color: {
-      type: 'string'
-    },
     position: {
       type: 'string',
       enum: ['fixed', 'absolute', 'sticky', 'static']
-    },
-    variant: {
-      variant: 'string',
-      enum: ['regular', 'dense']
     }
-
   },
   required: ['position']
 }
