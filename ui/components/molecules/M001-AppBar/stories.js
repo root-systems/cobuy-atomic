e195f60ac282e8c6a90f36f0b603dc37a6560cb7
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-23 12:50:04 +0200

    update story actions to use storybook action logger

--- ui/components/molecules/M001-AppBar/stories.js
+++ ui/components/molecules/M001-AppBar/stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
 // Import index.js from the same folder
 import AppBar from '.'
@@ -8,7 +9,7 @@ import AppBar from '.'
 storiesOf('Molecules|M001 - AppBar', module).add('Default', () => (
   <AppBar
     pageTitle='Cobuy'
-    menuClick={console.log('menu click')}
-    profileClick={console.log('profile click')}
+    menuClick={action('menu-click')}
+    profileClick={action('profile-click')}
   />
 ))
