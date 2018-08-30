20891d2db12eed015a2aaa00846575ce23d1ce33
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-30 10:02:37 +0200

    add readme with api notes so we dont have to look it up all the time...

--- ui/components/atoms/A010-InputWithLabel/stories.js
+++ ui/components/atoms/A010-InputWithLabel/stories.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withNotes } from '@storybook/addon-notes'
 
 // Import index.js from the same folder
 import Input from '.'
+import notes from './readme.md'
 
 // List of stories of use cases for atom
-storiesOf('Atoms|A010 - Input:Label', module).add('Input', () => (
-  <Input
-    id='1'
-    type='text'
-    name='fullName'
-    placeholder='Daniel Lewis'
-    label='Your wonderful name'
-  />
-))
+storiesOf('Atoms|A010 - Input:Label', module)
+  .addDecorator(withNotes)
+  .add('Input', () =>
+    <Input
+      id='1'
+      type='text'
+      name='fullName'
+      placeholder='Daniel Lewis'
+      label='Your wonderful name'
+    />,
+  {notes: { markdown: notes }
+  })
