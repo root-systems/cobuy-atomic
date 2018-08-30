5a668833af4555c15f1d2de09e6e50590a7e7500
Daniel Lewis <dan.lewis@enspiral.com>
2018-08-30 10:02:59 +0200

    swap order aroundto function as designed

--- ui/components/atoms/A014-Avatar/index.js
+++ ui/components/atoms/A014-Avatar/index.js
@@ -1,23 +1,25 @@
 import React, { Fragment } from 'react'
 import createPropTypes from 'json-schema-prop-types'
-import {isNil} from 'lodash'
+import {isNil, isEmpty} from 'lodash'
 
 import Avatar from '@material-ui/core/Avatar'
 import Icon from '../../particles/P001-Icon'
 
 import schema from './schema'
 
+const isNothing = (arg) => isNil(arg) || isEmpty(arg)
+
 const A014 = props => {
   return (
     <Fragment>
-      { isNil(props.iconname) && isNil(props.text) ? (
-        <Avatar alt={props.alt} src={props.src} srcSet={props.srcSet} />
-      ) : isNil(props.iconname) ? (
+      { isNothing(props.src) && isNil(props.text) ? (
+        <Avatar>
+          <Icon iconname={props.iconname} />
+        </Avatar>
+      ) : isNil(props.src) ? (
         <Avatar>{props.text}</Avatar>
       ) : (
-        <Avatar>
-          <Icon iconname='person' />
-        </Avatar>
+        <Avatar alt={props.alt} src={props.src} srcSet={props.srcSet} />
       )}
     </Fragment>
   )
