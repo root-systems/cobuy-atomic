3c2ce360a389299383626b6aa086667f7ec21379
Daniel Lewis <dan.lewis@enspiral.com>
2018-09-04 19:03:46 +0200

    update schema and story

--- ui/components/molecules/M003-ResponsiveDrawer/schema.js
+++ ui/components/molecules/M003-ResponsiveDrawer/schema.js
@@ -2,6 +2,25 @@ export default {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
   properties: {
-
-  }
+    open: {
+      type: 'boolean'
+    },
+    handleDrawerToggle: {
+      instanceOf: 'function'
+    },
+    children: {
+      anyOf: [
+        {
+          type: 'array',
+          items: {
+            type: 'object'
+          }
+        },
+        {
+          type: 'object'
+        }
+      ]
+    }
+  },
+  required: ['open', 'handleDrawerToggle', 'children']
 }
