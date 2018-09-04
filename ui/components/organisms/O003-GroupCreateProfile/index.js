f469f65a65713ac3c52f50a56b557ae52a8fbcf6
Daniel Lewis <dan.lewis@enspiral.com>
2018-09-04 10:52:45 +0200

    update to use form fragment

--- ui/components/organisms/O003-GroupCreateProfile/index.js
+++ ui/components/organisms/O003-GroupCreateProfile/index.js
@@ -1,100 +1,46 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import createPropTypes from 'json-schema-prop-types'
 
 import { Grid, Paper, withStyles } from '@material-ui/core/'
 
-import TextFieldLabel from '../../atoms/A010-TextFieldLabel'
-import TextAreaLabel from '../../atoms/A012-TextAreaLabel'
-import Avatar from '../../atoms/A018-Avatar'
+import ProfileFormFragment from '../../molecules/M002-ProfleFormFragment'
+
 import Button from '../../atoms/A001-Button'
 import Typography from '../../particles/P002-Typography'
-
-import AppBar from '../../molecules/M001-AppBar'
 
 import schema from './schema'
 import styles from './styles'
 
 const O003 = props => {
   return (
-    <Fragment>
-      <AppBar position='fixed' />
-      <Paper className={props.classes.paper} elevation={1}>
-        <Typography variant='headline' align={props.pageTitle.align}>
-          {props.pageTitle.title}
-        </Typography>
+    <Paper className={props.classes.paper} elevation={1}>
+      <Typography variant='headline' align='center'>
+        Create a group
+      </Typography>
 
-        <Grid container spacing={8}>
-          <Grid item xs={12}>
-            {/* TODO: onclick(Avatar) open upload modal when avatar clicked */}
-            <Avatar
-              alt={props.avatar.alt}
-              src={props.avatar.src}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextFieldLabel
-              id='uploadavatar'
-              name='uploadAvatar'
-              type='file'
-              error={props.uploadAvatarField.error}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextFieldLabel
-              id='name'
-              name='name'
-              label='Name'
-              placeholder='Your fullname'
-              type='text'
-              required
-              error={props.nameField.error}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextAreaLabel
-              id='description'
-              name='description'
-              label='Description'
-              placeholder='There once was a little group that wished to buy together...'
-              type='text'
-              multiline={props.descriptionField.multiline}
-              row={10}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextFieldLabel
-              id='website'
-              name='website'
-              label='Website address'
-              placeholder='www.website.com'
-              type='url'
-              error={props.websiteField.error}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextFieldLabel
-              id='email'
-              name='email'
-              label='Email address'
-              placeholder='support@cobuy.nz'
-              type='email'
-              error={props.emailField.error}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant='default'
-              color={props.cancelButton.color}
-              text={props.cancelButton.buttontitle}
-            />
-            <Button variant='contained'
-              color={props.okButton.color}
-              text={props.okButton.buttontitle}
-            />
-          </Grid>
+      <ProfileFormFragment
+        avatar={{ alt: props.avatar.alt, src: props.avatar.src }}
+        nameField={{
+          value: props.nameField.value,
+          error: props.nameField.error
+        }}
+        descriptionField={{ value: props.descriptionField.value }}
+        websiteField={{
+          value: props.websiteField.value,
+          error: props.websiteField.error
+        }}
+        emailField={{
+          value: props.emailField.value,
+          error: props.emailField.error
+        }}
+      />
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Button variant='default' color='secondary' text='Cancel' />
+          <Button variant='contained' color='primary' text='Save' />
         </Grid>
-      </Paper>
-      {/* TODO: Footer */}
-    </Fragment>
+      </Grid>
+    </Paper>
   )
 }
 
--- ui/components/organisms/O003-GroupCreateProfile/stories.js
+++ ui/components/organisms/O003-GroupCreateProfile/stories.js
@@ -3,28 +3,26 @@ import { storiesOf } from '@storybook/react'
 import { withKnobs, text, boolean } from '@storybook/addon-knobs'
 
 // Import index.js from the same folder
-import ProfleFormFragment from '.'
+import GroupCreateProfile from '.'
 
 // List of stories of use cases for atom
-storiesOf('Organisms|O003 - Group: Create Profile Form', module)
+storiesOf('Organisms|O003 - Create Group Profile', module)
   .addDecorator(withKnobs)
   .add('Default', () => (
-    <ProfleFormFragment
+    <GroupCreateProfile
       avatar={{
         alt: 'Avatar Alt Text',
         src: text('Avatar image src', 'http://www.catgifpage.com/gifs/318.gif')
-      }}
-      uploadAvatarField={{
-        error: boolean('Avatar error', false)
       }}
       nameField={{
         value: text('Name', 'John Snow'),
         error: boolean('Name error', false)
       }}
       descriptionField={{
-        value: text('Description', `The saddest aspect of life right now is that science fiction gathers knowledge faster than society gathers wisdom. - Isaac Asimov`),
-        multiline: boolean('multiline', true),
-        row: text('row', 10)
+        value: text(
+          'Description',
+          `The saddest aspect of life right now is that science fiction gathers knowledge faster than society gathers wisdom. - Isaac Asimov`
+        )
       }}
       websiteField={{
         value: text('Website Url', 'rootsystems.nz'),
@@ -33,18 +31,6 @@ storiesOf('Organisms|O003 - Group: Create Profile Form', module)
       emailField={{
         value: text('Email', 'hello@makebamboo.com'),
         error: boolean('Email error', false)
-      }}
-      okButton={{
-        color: text('color', 'primary'),
-        buttontitle: text('text', 'Ok')
-      }}
-      cancelButton={{
-        color: text('color', 'secondary'),
-        buttontitle: text('text', 'Cancel')
-      }}
-      pageTitle={{
-        title: text('title', 'Enter new group details'),
-        align: text('align', 'center')
       }}
     />
   ))
