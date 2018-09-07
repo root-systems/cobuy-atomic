d4db8b75b88ed6632dd2371eb118491237872926
Richard Garcia <richardgarciaph@gmail.com>
2018-09-07 16:46:49 +1200

    update M007 schema

--- ui/components/molecules/M007-AdressFormFragment/schema.js
+++ ui/components/molecules/M007-AdressFormFragment/schema.js
@@ -2,9 +2,57 @@ export default {
   $schema: 'http://json-schema.org/schema#',
   type: 'object',
   properties: {
+
     addressField01: {
       type: 'object',
       properties: {
+        value: {
+          type: 'string'
+        },
+        error: {
+          type: 'boolean'
+        }
+      }
+    },
+    suburbField: {
+      type: 'object',
+      properties: {
+        value: {
+          type: 'string'
+        },
+        error: {
+          type: 'boolean'
+        }
+      }
+    },
+    cityField: {
+      type: 'object',
+      properties: {
+        value: {
+          type: 'string'
+        },
+        error: {
+          type: 'boolean'
+        }
+      }
+    },
+    postCodeField: {
+      type: 'object',
+      properties: {
+        value: {
+          type: 'string'
+        },
+        error: {
+          type: 'boolean'
+        }
+      }
+    },
+    countryField: {
+      type: 'object',
+      properties: {
+        value: {
+          type: 'string'
+        },
         error: {
           type: 'boolean'
         }
