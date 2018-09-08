cf1ed31281a399605a6a59876ad080dd36acb8da
Daniel Lewis <dan.lewis@enspiral.com>
2018-09-08 09:44:15 +0200

    switch to using hoc pattern - not working yet :(

--- ui/components/organisms/O003-GroupCreateProfile/index.js
+++ ui/components/organisms/O003-GroupCreateProfile/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import createPropTypes from 'json-schema-prop-types'
-import { Formik } from 'formik'
+import { withFormik } from 'formik'
 import { string, object } from 'yup'
 import { isNil } from 'lodash'
 
@@ -14,7 +14,7 @@ import Typography from '../../particles/P002-Typography'
 import schema from './schema'
 import styles from './styles'
 
-const loadValues = (props) => ({
+const loadValues = props => ({
   avatarAlt: isNil(props.avatarAlt) ? '' : props.avatarAlt,
   avatarSrc: isNil(props.avatarSrc) ? '' : props.avatarSrc,
   nameField: isNil(props.name) ? '' : props.name,
@@ -23,11 +23,12 @@ const loadValues = (props) => ({
   emailField: isNil(props.email) ? '' : props.email
 })
 
-const validateSchema = () => object().shape({
-  name: string().required(),
-  email: string().email(),
-  website: string().url()
-})
+const validateSchema = () =>
+  object().shape({
+    name: string().required(),
+    email: string().email(),
+    website: string().url()
+  })
 
 const handleSubmit = (values, { setSubmitting }) => {
   setTimeout(() => {
@@ -36,55 +37,70 @@ const handleSubmit = (values, { setSubmitting }) => {
   }, 400)
 }
 
-const O003 = props => {
-  const { classes, handleCancel } = props
-  return (
-    <Formik
-      initialValues={loadValues(props)}
-      validationSchema={validateSchema}
-      onSubmit={handleSubmit}
-    >
-      {({
-        values,
-        errors,
-        handleChange,
-        handleSubmit,
-        isSubmitting
-        /* Todo: pass isSubmitting to Save/Send button */
-      }) => (
-        <Paper className={classes.paper} elevation={1}>
-          <Typography variant='headline' align='center'>
-            Create a group
-          </Typography>
+const formikEnhancer = withFormik({
+  initialValues: loadValues,
+  mapPropsToValues: props => ({
+    avatarAlt: props.avatarAlt,
+    avatarSrc: props.avatarSrc,
+    nameField: props.name,
+    descriptionField: props.description,
+    websiteField: props.website,
+    emailField: props.email
+  }),
+  validationSchema: validateSchema,
+  onSubmit: handleSubmit
+})
 
-          <ProfileFormFragment
-            onChange={handleChange}
-            avatar={{ alt: values.avatarAlt, src: values.avatarSrc }}
-            nameField={{
-              value: values.nameField,
-              error: errors.nameField
-            }}
-            descriptionField={{ value: values.descriptionField }}
-            websiteField={{
-              value: values.websiteField,
-              error: errors.websiteField
-            }}
-            emailField={{
-              value: values.emailField,
-              error: errors.emailField
-            }}
+const form = props => {
+  const {
+    classes,
+    handleCancel,
+    values,
+    errors,
+    handleChange,
+    handleSubmit
+    /* Todo: pass isSubmitting to Save/Send button
+    isSubmitting
+    */
+  } = props
+  return (
+    <Paper className={classes.paper} elevation={1}>
+      <Typography variant='headline' align='center'>
+        Create a group
+      </Typography>
+      <ProfileFormFragment
+        onChange={handleChange}
+        avatar={{ alt: values.avatarAlt, src: values.avatarSrc }}
+        nameField={{
+          value: values.nameField,
+          error: errors.nameField
+        }}
+        descriptionField={{ value: values.descriptionField }}
+        websiteField={{
+          value: values.websiteField,
+          error: errors.websiteField
+        }}
+        emailField={{
+          value: values.emailField,
+          error: errors.emailField
+        }}
+      />
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Button onClick={handleCancel} color='secondary' text='Cancel' />
+          <Button
+            onClick={handleSubmit}
+            variant='contained'
+            color='primary'
+            text='Save'
           />
-          <Grid container spacing={8}>
-            <Grid item xs={12}>
-              <Button onClick={handleCancel} color='secondary' text='Cancel' />
-              <Button onClick={handleSubmit} variant='contained' color='primary' text='Save' />
-            </Grid>
-          </Grid>
-        </Paper>
-      )}
-    </Formik>
+        </Grid>
+      </Grid>
+    </Paper>
   )
 }
+
+const O003 = formikEnhancer(form)
 
 O003.propTypes = createPropTypes(schema)
 
