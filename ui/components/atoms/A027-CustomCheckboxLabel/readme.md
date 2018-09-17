6554b51cad4858ca9091f3ced1d18de772ea3cef
Richard Garcia <richardgarciaph@gmail.com>
2018-09-17 16:18:36 +1200

    initial code for A027

---
+++ ui/components/atoms/A027-CustomCheckboxLabel/readme.md
@@ -0,0 +1,1 @@
+checkbox  with custom icon
---
+++ ui/components/atoms/A027-CustomCheckboxLabel/stories.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withNotes } from '@storybook/addon-notes'
+
+// Import index.js from the same folder
+import CheckboxLabel from '.'
+import notes from './readme.md'
+
+// List of stories of use cases for atom
+storiesOf('Atoms|A027 - CustomCheckboxLabel', module)
+  .addDecorator(withNotes)
+  .add('Checkbox Default', () => (
+    <CheckboxLabel
+      label='Checkers'
+      value='checkboxValue'
+      id='check1'
+      ariaLabel='Check Default'
+    />
+  ), { notes: { markdown: notes } })
+  .add('Checkbox Checked', () => (
+    <CheckboxLabel
+      label='I have a dog'
+      value='checkboxValue'
+      id='check2'
+      ariaLabel='Check your check'
+      checked
+    />
+  ))
+  .add('Checkbox Label left', () => (
+    <CheckboxLabel
+      label='I have a dog'
+      value='checkboxValue'
+      id='check2'
+      ariaLabel='Check your check'
+      checked
+      labelPlacement='start'
+    />
+  ))
+  .add('Checkbox Disabled', () => (
+    <CheckboxLabel
+      label='But I love cats'
+      value='checkboxValue'
+      id='check3'
+      ariaLabel='Disabled Check'
+      disabled
+    />
+  ))
