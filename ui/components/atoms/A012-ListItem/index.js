54232e8bcc49ba4f1d0aa9d157339d02d44d17c7
Richard Garcia <richardgarciaph@gmail.com>
2018-09-24 20:23:06 +1200

    update List item atom, A012

--- ui/components/atoms/A012-ListItem/index.js
+++ ui/components/atoms/A012-ListItem/index.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import createPropTypes from 'json-schema-prop-types'
+import { isNil, isEmpty } from 'lodash'
 
 import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import Icon from '../../particles/P001-Icon'
 
 import schema from './schema'
 
 const A012 = props => {
-  const { children } = props
+  const isNothing = arg => isNil(arg) || isEmpty(arg)
+  const { iconname, primarytext, secondarytext } = props
 
   return (
-    <ListItem button>
-      {children}
-    </ListItem>
+    <Fragment>
+      {isNothing(primarytext) ? (
+        <ListItem button>
+          <Icon iconname={iconname} />
+        </ListItem>
+      ) : isNil(iconname) ? (
+        <ListItem button>
+          <ListItemText
+            primary={primarytext}
+            secondary={secondarytext}
+          />
+        </ListItem>
+      ) : (
+        <ListItem button>
+          <Icon iconname={iconname} />
+          <ListItemText
+            primary={primarytext}
+            secondary={secondarytext}
+          />
+        </ListItem>
+      )}
+    </Fragment>
   )
 }
 
 A012.propTypes = createPropTypes(schema)
-
 export default A012
--- ui/components/atoms/A012-ListItem/stories.js
+++ ui/components/atoms/A012-ListItem/stories.js
@@ -3,29 +3,29 @@ import { storiesOf } from '@storybook/react'
 
 // Import index.js from the same folder
 import ListItem from '.'
-import ListItemText from '../A011-ListItemText'
-import Icon from '../../particles/P001-Icon'
 
 // List of stories of use cases for atom
 storiesOf('Atoms|A012 - List Item', module)
-  .add('Primary text', () => (
-    <ListItem>
-      <ListItemText primary='List item text' />
-    </ListItem>
-  ))
-  .add('Primary & Secondary text', () => (
-    <ListItem>
-      <ListItemText primary='List item text' secondary='Secondary text' />
-    </ListItem>
-  ))
-  .add('Primary text & Icon', () => (
-    <ListItem>
-      <Icon iconname='settings' />
-      <ListItemText primary='Settings' />
-    </ListItem>
-  ))
   .add('Icon only', () => (
-    <ListItem>
-      <Icon iconname='settings' />
-    </ListItem>
+    <ListItem
+      iconname='add'
+    />
+  ))
+  .add('Primary text only', () => (
+    <ListItem
+      primarytext='primary text only'
+    />
+  ))
+  .add('Primary and Secondary text', () => (
+    <ListItem
+      primarytext='Primary text'
+      secondarytext='Secondary text'
+    />
+  ))
+  .add('Show all elements', () => (
+    <ListItem
+      iconname='add'
+      primarytext='Primary text (all)'
+      secondarytext='Secondary text (all)'
+    />
   ))
