eabcf7b8c3394311e6f75338e9b89aa8af714b0a
Richard Garcia <richardgarciaph@gmail.com>
2018-09-24 18:18:34 +1200

    adding button & primary text logic

--- ui/components/atoms/A012v2-ListItemV2/stories.js
+++ ui/components/atoms/A012v2-ListItemV2/stories.js
@@ -6,9 +6,34 @@ import ListItem from '.'
 
 // List of stories of use cases for atom
 storiesOf('Atoms|A012v2 - List Item', module)
-  .add('Primary text', () => (
+  .add('Icon only', () => (
     <ListItem
-      primarytext='List item text'
       iconname='add'
     />
   ))
+  .add('show all', () => (
+    <ListItem
+      iconname='add'
+      secondarytext='Secondary text all'
+    />
+  ))
+  // .add('Primary text only', () => (
+  //   <ListItem
+  //     iconname='ad'
+  //     primarytext='Primary text'
+  //     secondarytext='Secondary text'
+  //   />
+  // ))
+  // .add('Icon & Primary text', () => (
+  //   <ListItem
+  //     primarytext='Primary text'
+  //     iconname='add'
+  //   />
+  // ))
+  // .add('Icon, Primary & Secondary text', () => (
+  //   <ListItem
+  //     iconname='add'
+  //     primarytext='Primary text'
+  //     secondarytext='Secondary text'
+  //   />
+  // ))
