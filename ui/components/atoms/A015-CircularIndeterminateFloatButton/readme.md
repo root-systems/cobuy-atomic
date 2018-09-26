dd82e693c16d3fb5b756f3f767f94c4c9bb83297
Richard Garcia <richardgarciaph@gmail.com>
2018-09-26 14:53:55 +1200

    delete A015, move floating icon progress in A002

--- ui/components/atoms/A015-CircularIndeterminateFloatButton/readme.md
+++
@@ -1,1 +0,0 @@
-Indeterminate state button
--- ui/components/atoms/A015-CircularIndeterminateFloatButton/styles.js
+++
@@ -1,9 +0,0 @@
-export default theme => ({
-  circularProgress: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    marginTop: -34,
-    marginLeft: -34
-  }
-})
